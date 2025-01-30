a77c9b8a33b244cf1d464537941d9f36f8fb8143
umbcoppolabottazzi <umberto.coppolabottazzi@pagopa.it>
2025-01-30 11:47:00 +0100

    fix role definitions

--- src/05_elastic_resources_common/02_role_definitions.tf
+++ src/05_elastic_resources_common/02_role_definitions.tf
@@ -55,7 +55,10 @@ resource "elasticstack_elasticsearch_security_role" "editor_role" {
       {
         application = "kibana-.kibana",
         privileges = [
-          "space_read"
+          "feature_visualize.all", "feature_discover.all", "feature_dashboard.all",
+          "feature_canvas.all", "feature_ml.all", "feature_graph.all", "feature_maps.all",
+          "feature_logs.all", "feature_infrastructure.all", "feature_apm.all", "feature_uptime.all",
+          "feature_slo.all", "feature_dev_tools.all", "feature_indexPatterns.read",
         ],
         resources = ["*"]
       }
@@ -79,23 +82,18 @@ resource "elasticstack_elasticsearch_security_role" "viewer_role" {
     content {
       names = [indices.key]
       privileges = [
-        "auto_configure", "create", "create_doc", "create_index", "delete",
-        "index", "maintenance", "manage", "manage_data_stream_lifecycle",
-        "manage_follow_index", "manage_leader_index", "monitor", "read",
-        "view_index_metadata", "write"
+        "monitor", "read", "view_index_metadata",
       ]
     }
   }
 
   dynamic "applications" {
     for_each = toset([
-      { application = "kibana-.kibana",
-        privileges = [
-          "feature_visualize.all", "feature_discover.all", "feature_dashboard.all",
-          "feature_canvas.all", "feature_ml.all", "feature_graph.all", "feature_maps.all",
-          "feature_logs.all", "feature_infrastructure.all", "feature_apm.all", "feature_uptime.all",
-        "feature_slo.all", "feature_dev_tools.all", "feature_indexPatterns.read"],
-      resources = ["*"] }
+      {
+        application = "kibana-.kibana",
+        privileges  = ["space_read"],
+        resources   = ["*"]
+      }
     ])
     content {
       application = applications.value.application
