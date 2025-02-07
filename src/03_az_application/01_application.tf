a29c112fdfe34a15f02ebb365e113189ae427739
mamari90 <130982006+mamari90@users.noreply.github.com>
2025-02-07 16:31:21 +0100

    added SP

--- src/03_az_application/01_application.tf
+++ src/03_az_application/01_application.tf
@@ -4,5 +4,11 @@ resource "azuread_application" "ec_application" {
   owners          = [data.azuread_client_config.current.object_id]
 }
 
+resource "azuread_service_principal" "ec_application_principal" {
+  client_id                    = azuread_application.ec_application.client_id
+  app_role_assignment_required = false
+  owners                       = [data.azuread_client_config.current.object_id]
+}
+
 #TODO Associazione tra gruppo AD (creato su eng-azure) con l'application
 # Come fare? Vedi https://github.com/pagopa/azuread-tf-modules/blob/main/assign_user_to_service_principal/main.tf
