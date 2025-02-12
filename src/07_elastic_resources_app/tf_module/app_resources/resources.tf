8bc3ef3d822f2f43e0b923fa68c3c3fabc6c89fb
mamari90 <130982006+mamari90@users.noreply.github.com>
2025-02-12 18:03:09 +0100

    wip

--- src/07_elastic_resources_app/tf_module/app_resources/resources.tf
+++ src/07_elastic_resources_app/tf_module/app_resources/resources.tf
@@ -118,6 +118,7 @@ resource "elasticstack_kibana_import_saved_objects" "dashboard" {
   space_id   = var.space_id
   file_contents = templatefile(each.value, {
     data_view = elasticstack_kibana_data_view.kibana_data_view.data_view.id
+    apm_static_data_view = "apm_static_data_view_id_${var.space_id}"
   })
 }
 
