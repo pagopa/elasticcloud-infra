c502423f849eab4411f882fa1b9cf7380d6f5f46
mamari90 <130982006+mamari90@users.noreply.github.com>
2025-03-31 11:06:41 +0200

    updated ilm configuration, changed defualt ilm name to avoid conflicts

--- src/06_elastic_resources_deployment/02_default_template_customization.tf
+++ src/06_elastic_resources_deployment/02_default_template_customization.tf
@@ -7,14 +7,14 @@ locals {
 
   custom_lifecycle_components = { for k, v in local.custom_licecycle_idx : k =>
     jsondecode(templatefile("${path.module}/custom_resources/index_component/basic-only-lifecycle@custom.json", {
-      lifecycle = "${var.prefix}-${var.env}-${v}-ilm",
+      lifecycle = "${local.ilm_prefix}-${v}-ilm",
       name      = k
       }
   )) }
 }
 
 resource "elasticstack_elasticsearch_component_template" "custom_components_default_index_lifecycle" {
-  depends_on = [elasticstack_elasticsearch_index_lifecycle.index_lifecycle]
+  depends_on = [elasticstack_elasticsearch_index_lifecycle.index_lifecycle, elasticstack_elasticsearch_index_lifecycle.deployment_index_lifecycle]
   for_each   = local.custom_lifecycle_components
 
   name = "${each.key}@custom"
