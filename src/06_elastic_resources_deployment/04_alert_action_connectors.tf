d75d92e0c417e67a5dcc15aafb8b4b77cbad0c09
Marco Mari <130982006+mamari90@users.noreply.github.com>
2025-05-07 10:17:41 +0200

    feat: Added custom app connectors (#82)

--- src/06_elastic_resources_deployment/04_alert_action_connectors.tf
+++ src/06_elastic_resources_deployment/04_alert_action_connectors.tf
@@ -24,3 +24,14 @@ resource "elasticstack_kibana_action_connector" "opsgenie" {
     apiUrl = "https://api.opsgenie.com"
   })
 }
+
+
+resource "elasticstack_kibana_action_connector" "app_connector" {
+  for_each          = var.app_connectors
+  name              = each.key
+  connector_type_id = ".${each.value.type}"
+  secrets = jsonencode(
+    each.value.type == "opsgenie" ? { apiKey = data.azurerm_key_vault_secret.app_connector_secret_key[each.key].value } : { webhookUrl = data.azurerm_key_vault_secret.app_connector_secret_key[each.key].value }
+  )
+  config = each.value.type == "opsgenie" ? jsonencode({ apiUrl = "https://api.opsgenie.com" }) : null
+}
