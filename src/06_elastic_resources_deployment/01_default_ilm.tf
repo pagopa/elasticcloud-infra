b2fff5830b8f4a6ff96fa0ea85914eb0121f0bdf
mamari90 <130982006+mamari90@users.noreply.github.com>
2025-06-06 10:50:40 +0200

    prepared index template customization

--- src/06_elastic_resources_deployment/01_default_ilm.tf
+++ src/06_elastic_resources_deployment/01_default_ilm.tf
@@ -34,62 +34,6 @@ locals {
 
 }
 
-# kept for backward compatibility
-# the policy name not containing "deployment" is deprecated, should be kept until all indices using it are deleted, then it can be removed
-resource "elasticstack_elasticsearch_index_lifecycle" "index_lifecycle" {
-  for_each = local.ilm_all_policies
-
-  name = "${local.prefix_env}-${each.key}-ilm"
-
-  hot {
-    min_age = each.value.hot.minAge
-
-    rollover {
-      max_primary_shard_size = each.value.hot.rollover.maxPrimarySize
-      min_primary_shard_size = lookup(each.value.hot.rollover, "minPrimarySize", null)
-      max_age                = each.value.hot.rollover.maxAge
-    }
-  }
-
-  warm {
-    min_age = each.value.warm.minAge
-
-    set_priority {
-      priority = each.value.warm.setPriority
-    }
-  }
-
-  dynamic "cold" {
-    for_each = lookup(each.value, "cold", null) != null ? [1] : []
-    content {
-      min_age = each.value.cold.minAge
-
-      set_priority {
-        priority = each.value.cold.setPriority
-      }
-    }
-  }
-
-  delete {
-    min_age = each.value.delete.minAge
-
-    delete {
-      delete_searchable_snapshot = each.value.delete.deleteSearchableSnapshot
-    }
-
-    dynamic "wait_for_snapshot" {
-      for_each = var.ilm_delete_wait_for_snapshot ? [1] : []
-      content {
-        policy = each.value.delete.waitForSnapshot
-      }
-    }
-  }
-
-  metadata = jsonencode({
-    "managedBy" = "Terraform"
-  })
-}
-
 
 # new ilm naming convention; uses ilm_prefix which contains the "deployment" string
 # name changed to avoid conflicts with ilm defined in 07_elastic_resources_app
--- src/06_elastic_resources_deployment/02_default_template_customization.tf
+++ src/06_elastic_resources_deployment/02_default_template_customization.tf
@@ -1,15 +1,15 @@
 locals {
   custom_licecycle_idx = {
-    logs : { lifecycle : "${local.ilm_prefix}-${var.default_ilm_logs}-ilm", primary_shard_count = var.primary_shard_count },
-    traces : { lifecycle : "${local.ilm_prefix}-${var.default_ilm_traces}-ilm", primary_shard_count = var.primary_shard_count },
-    metrics : { lifecycle : "${local.ilm_prefix}-${var.default_ilm_metrics}-ilm", primary_shard_count = var.primary_shard_count },
-    elastic : { lifecycle : "${local.ilm_prefix}-elastic-ilm", primary_shard_count = var.primary_shard_count },
-    metricbeat : { lifecycle : "${local.ilm_prefix}-metricbeat-ilm", primary_shard_count = var.primary_shard_count },
-    elastic_monitoring : { lifecycle : "${local.ilm_prefix}-elastic_monitoring-ilm", primary_shard_count = var.primary_shard_count }
+    logs : { lifecycle : "${local.ilm_prefix}-${var.default_ilm_logs}-ilm", primary_shard_count = var.primary_shard_count, component = "lifecycle-and-shard@custom.json" },
+    traces : { lifecycle : "${local.ilm_prefix}-${var.default_ilm_traces}-ilm", primary_shard_count = var.primary_shard_count, component = "lifecycle-and-shard@custom.json" },
+    metrics : { lifecycle : "${local.ilm_prefix}-${var.default_ilm_metrics}-ilm", primary_shard_count = var.primary_shard_count, component = "lifecycle-and-shard@custom.json" },
+    elastic : { lifecycle : "${local.ilm_prefix}-elastic-ilm", primary_shard_count = var.primary_shard_count, component = "lifecycle-and-shard@custom.json" },
+    metricbeat : { lifecycle : "${local.ilm_prefix}-metricbeat-ilm", primary_shard_count = var.primary_shard_count, component = "lifecycle-and-shard@custom.json" },
+    elastic_monitoring : { lifecycle : "${local.ilm_prefix}-elastic_monitoring-ilm", primary_shard_count = var.primary_shard_count, component = "lifecycle-and-shard@custom.json" }
   }
 
   custom_lifecycle_components = { for k, v in local.custom_licecycle_idx : k =>
-    jsondecode(templatefile("${path.module}/custom_resources/index_component/lifecycle-and-shard@custom.json", {
+    jsondecode(templatefile("${path.module}/custom_resources/index_component/${v.component}", {
       lifecycle           = v.lifecycle,
       name                = k
       primary_shard_count = try(v.primary_shard_count, 1)
@@ -18,7 +18,7 @@ locals {
 }
 
 resource "elasticstack_elasticsearch_component_template" "custom_components_default_index_lifecycle" {
-  depends_on = [elasticstack_elasticsearch_index_lifecycle.index_lifecycle, elasticstack_elasticsearch_index_lifecycle.deployment_index_lifecycle]
+  depends_on = [elasticstack_elasticsearch_index_lifecycle.deployment_index_lifecycle]
   for_each   = local.custom_lifecycle_components
 
   name = "${each.key}@custom"
