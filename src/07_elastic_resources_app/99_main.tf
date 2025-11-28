53cbe19eb884a1b8ae59f26100e320d8874aded4
mamari90 <130982006+mamari90@users.noreply.github.com>
2025-11-28 17:50:29 +0100

    update

--- src/07_elastic_resources_app/99_main.tf
+++ src/07_elastic_resources_app/99_main.tf
@@ -46,5 +46,5 @@ provider "elasticstack" {
 
 module "__v4__" {
   # 7.28.0
-  source = "git::https://github.com/pagopa/terraform-azurerm-v4?ref=17ff2b9"
+  source = "git::https://github.com/pagopa/terraform-azurerm-v4?ref=64a9244"
 }
