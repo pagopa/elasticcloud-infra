96d997fa2ab6a1f6204966aef076395f62ae2181
Marco Mari <130982006+mamari90@users.noreply.github.com>
2026-09-16 10:56:06 +0200

    [PIDM-2413] fix dashboard configuration

--- src/06_elastic_resources_common/02_role_definitions.tf
+++ src/06_elastic_resources_common/02_role_definitions.tf
@@ -52,7 +52,7 @@ resource "elasticstack_elasticsearch_security_role" "editor_role" {
           "feature_visualize.all", "feature_discover.all", "feature_dashboard.all",
           "feature_canvas.all", "feature_ml.all", "feature_graph.all", "feature_maps.all",
           "feature_logs.all", "feature_infrastructure.all", "feature_apm.all", "feature_uptime.all",
-          "feature_slo.all", "feature_dev_tools.all", "feature_indexPatterns.read", "feature_savedObjectsManagement.read"
+          "feature_slo.all", "feature_dev_tools.all", "feature_indexPatterns.read", "feature_savedObjectsManagement.read", "feature_streams.read"
         ],
         resources = ["space:default", "space:*-${var.env}"]
       }
